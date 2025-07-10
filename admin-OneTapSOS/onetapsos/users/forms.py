4aa4f7990bc27410385333d20d09cc57258b5e86
Project Raven <raven.dev2024@gmail.com>
2025-07-11 04:27:27 +0800

    Register Page

---
+++ admin-OneTapSOS/onetapsos/users/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class RegisterForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(required=True)
+    phone_number = forms.CharField(max_length=15, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
+    confirm_password = forms.CharField(widget=forms.PasswordInput, required=True)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+        if password and confirm_password and password != confirm_password:
+            self.add_error('confirm_password', "Passwords do not match.")
+        return cleaned_data
+
+"""
+ Note: The default Django User model does not have a phone_number field. For now, this will be handled as a form field only. For production, consider a custom user model or a profile model.
+"""
